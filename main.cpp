ea144b2f3f1ce1876969ffe77b340e126be62bae
fhadnagy <fonagymarci@gmail.com>
2022-02-05 19:10:56 +0100

    dragon

--- main.cpp
+++ main.cpp
@@ -1,31 +1,54 @@
 #include "turtlefractals.h"
-/*
-class Apple
-{
-private:
-    std::string pstr = "";
 
-public:
-    Apple(std::string _str) : pstr(_str) {}
-    ~Apple() { std::cout << "destruct" << std::endl; }
-    void setps(const std::string &st);
-    void getps();
-};
+std::string one = "-";
 
-void Apple::setps(const std::string &st)
-{
-    pstr = st;
-}
-void Apple::getps()
-{
-    std::cout << "\n"
-              << pstr << std::endl;
-}
-*/
+std::string doublereverse(const std::string &str);
+
 int main(int argc, char const *argv[])
 {
     std::cout << "start" << std::endl;
-    Turns trn("tr","tl","fw","bw","pu","pd",78,50);
-    trn.iterate("trns.txt",40);
+    Turtle t;
+    // Turns trn("tr", "tl", "fw", "bw", "pu", "pd", 78, 50);
+    // trn.iterate("trns.txt", 40);
+    for (int i = 0; i < 20; i++)
+    {
+        one = one + '-' + doublereverse(one);
+    }
+    t.opf("dragon.txt");
+    for (int i = 0; i < one.size() - 1; i++)
+    {
+        t.fw(4);
+        switch (one[i])
+        {
+        case '-':
+            t.lt(90);
+            break;
+        case '+':
+            t.rt(90);
+            break;
+        default:
+            break;
+        }
+    }
+    t.fw(4);
+    t.clf();
     return 0;
+}
+
+std::string doublereverse(const std::string &str)
+{
+    std::string op = "";
+    for (int i = str.size() - 1; i >= 0; i--)
+    {
+        if (str[i] == '-')
+        {
+            op += "+";
+        }
+        else
+        {
+            op += "-";
+        }
+    }
+
+    return op;
 }
